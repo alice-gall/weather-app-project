054397daa8d867d2234a9979e7987f295d804b9a
alicegall <alicegall38@gmail.com>
2026-05-21 11:34:17 +0100

    Added API to the search engine functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function displayWeather(response) {
+  let currentTemperatureValue = document.querySelector("#temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  currentTemperatureValue.innerHTML = temperature;
+  let cityTitleElement = document.querySelector("h1");
+  let city = response.data.city;
+  cityTitleElement.innerHTML = city;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-bar");
-  let city = document.querySelector("h1");
-  city.innerHTML = searchInput.value;
+
+  let apiKey = "d043896d344020fed4co182b1f4a0tb8";
+  let cityTitle = searchInput.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityTitle}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayWeather);
 }
 
 let searchForm = document.querySelector("#search-form");
